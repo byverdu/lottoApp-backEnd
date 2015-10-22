40513614b8ee23581979a2a0621f17b2f519dd6a
byverdu <byverdu@gmail.com>
2015-10-22 01:58:29 +0100

    working example second winner instance with xray and saving on db

---
+++ app/instances/bonoWinner.js
@@ -0,0 +1,53 @@
+'use strict';
+
+import mongoose from 'mongoose';
+import Winner from '../model/winnerSchema';
+import {GlobalHelper} from '../helpers/globalHelper';
+let storage = require('../config/storage'),
+globalHelper = new GlobalHelper();
+
+module.exports = () => {
+  require('../config/db')();
+
+  console.log('instances file called bonolotoWinner');
+
+  var db = mongoose.connection;
+
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function() {
+
+    console.log('open connection bonolotoWinner');
+
+    globalHelper.customFindOneMongoose(Winner, {lottoID: 'bonolotoWinner'}, (err, winner) => {
+
+      if (err) {
+        console.log(err);
+      } else {
+
+        let bonoStorage = storage.getItem('bonoWinners'),
+          oldWinner = winner.allWinners[4].winners,
+          newWinner = bonoStorage.allWinners[4].winners;
+
+          console.log(oldWinner, newWinner);
+
+        if (oldWinner !== newWinner) {
+
+          winner.date = new Date();
+          winner.allWinners = bonoStorage.allWinners;
+          winner.extraInfo = bonoStorage.extraInfo;
+          winner.save((err, winner) => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log(winner, 'saved bonolotoWinner');
+            }
+          });
+        }
+
+      }
+    });
+  });
+  setTimeout(() => {
+    mongoose.disconnect();
+  }, 1000);
+};
---
+++ app/lottoXray/bonoWinnerXray.js
@@ -0,0 +1,42 @@
+'use strict';
+
+import Xray from '../helpers/xray';
+import {GlobalHelper} from '../helpers/globalHelper';
+import storage from '../config/storage';
+let xray = new Xray(),
+  globalHelper = new  GlobalHelper(),
+  configBono = require('../config/config')().lotto.bonoloto;
+
+module.exports = () => {
+  console.log('bonoWinnerXray file called');
+
+  xray.get(configBono.urlPrice, {
+    categoryPrice: [configBono.categoryPrice],
+    winnerPrice: [configBono.winnerPrice],
+    moneyPrice: [configBono.moneyPrice],
+    extraInfoPrice: [configBono.extraInfoPrice]
+  }).then(result => {
+
+    let newWinner = globalHelper.getPricesInfo(result),
+      fourthWinner = newWinner[4].winners,
+      oldWinner = storage.getItem('bonoWinners');
+
+      if (fourthWinner !== oldWinner.allWinners[4].winners){
+
+        let newStorage = {
+          allWinners: newWinner,
+          extraInfo: result.extraInfoPrice
+        };
+
+        storage.setItem('bonoWinners', newStorage).then(
+          ()=>{
+            console.log('setItem for bonoWinners');
+            require('../instances/bonoWinner')();
+          },()=>{
+            console.log('error setting bonoWinners');
+          });
+      }
+
+      console.log('bonoWinners xray end call');
+  });
+};
