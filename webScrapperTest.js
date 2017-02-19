a1c3013d0e7cd84d019a6d6153a891d26aa3a86d
byverdu <byverdu@gmail.com>
2017-02-19 10:39:37 +0000

    added *generator :rocket:

--- webScrapperTest.js
+++ webScrapperTest.js
@@ -3,32 +3,61 @@
 import Xray from './app/config/xray';
 const xray = new Xray();
 const config = require( './app/config/config' ).lotto;
-const raffleTypes = ['bonoloto', 'primitiva', 'euromillions'];
+const iterations = 6;
 
-const getUrlFor = ( raffle ) => {
+function getUrlFor( raffle ) {
   const { url, urlPrice } = config[ raffle ];
 
   return {
     url,
     urlPrice
   };
-};
+}
 
-const getSelectorsFor = ( raffle ) => {
+function getSelectorsFor( raffle ) {
   const { numbers, moneyPrice } = config[ raffle ];
 
   return {
     numbers,
     moneyPrice
   };
-};
+}
 
-for ( const raffle of raffleTypes ) {
+function getNumberValuesFor( raffle ) {
+  const url = getUrlFor( raffle ).url;
+  const selector = getSelectorsFor( raffle ).numbers;
   xray.get(
-    getUrlFor( raffle ).url, {
-      numbers: [getSelectorsFor( raffle ).numbers]
+    url, {
+      numbers: [selector]
     }
-  ).then(
-    result => console.log( `${raffle}: ${result.numbers}` )
+  ).then( result =>
+    console.log( `${raffle}: ${result.numbers}` )
   );
+}
+
+function getPriceValuesFor( raffle ) {
+  const url = getUrlFor( raffle ).urlPrice;
+  const selector = getSelectorsFor( raffle ).moneyPrice;
+  xray.get(
+    url, {
+      moneyPrice: [selector]
+    }
+  ).then( result =>
+    console.log( `${raffle}: ${result.moneyPrice}` )
+  );
+}
+
+
+function *executeXray() {
+  yield getNumberValuesFor( 'bonoloto' );
+  yield getPriceValuesFor( 'bonoloto' );
+  yield getNumberValuesFor( 'primitiva' );
+  yield getPriceValuesFor( 'primitiva' );
+  yield getNumberValuesFor( 'euromillions' );
+  yield getPriceValuesFor( 'euromillions' );
+}
+
+const executor = executeXray();
+for ( let i = 0; i < iterations; i++ ) {
+  executor.next();
 }
