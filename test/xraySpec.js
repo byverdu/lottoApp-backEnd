0b114cbdef24be2dc08f49f1bd2c70f3122cc16f
byverdu <byverdu@gmail.com>
2017-02-21 23:52:42 +0000

    changed function to class, sinon wasn't like it

--- test/xraySpec.js
+++ test/xraySpec.js
@@ -1,24 +1,59 @@
+// initial setup
 import sinon from 'sinon';
 import chai from 'chai';
-import xray from 'x-ray';
-const config = require( '../app/config/config' ).lotto.euromillions;
-const { url, numbers, extras } = config;
-
+import globalXray from 'x-ray';
 const expect = chai.expect;
-function xrayWrapper() {
-  return new Promise(( resolve, reject ) => {
+const config = require( '../app/config/config' ).lotto.euromillions;
+class XrayWrapper {
+  getValues( urlRaffle, selector ) {
+    return new Promise(( resolve, reject ) => {
+      const xray = globalXray();
 
-  });
+      xray( urlRaffle, selector )(( error, result ) => {
+        if ( error ) {
+          reject( error );
+        } else {
+          resolve( result );
+        }
+      });
+    });
+  }
 }
+// variables used
+const {
+  url,
+  numbers,
+  extras,
+  sliceCountBall
+} = config;
+const selectors = {
+  numbers: [numbers],
+  extras: [extras]
+};
+let spy;
+let xrayWrapper;
+
+beforeEach(() => {
+  xrayWrapper = new XrayWrapper();
+});
 
 describe( 'wrapper for xray web scrapper', () => {
   it( 'is defined', () => {
     expect( xrayWrapper ).not.to.eq( undefined );
   });
-  it( 'is a Function', () => {
-    expect( xrayWrapper ).to.be.instanceof( Function );
+  it( 'is an instanceof XrayWrapper', () => {
+    expect( xrayWrapper ).to.be.instanceof( XrayWrapper );
   });
-  it( 'returns a Promise', () => {
-    expect( xrayWrapper()).to.be.instanceof( Promise );
+  it( 'has a getValues property', () => {
+    expect( xrayWrapper.getValues ).not.to.eq( undefined );
+  });
+  it( 'getValues returns a Promise', () => {
+    expect( xrayWrapper.getValues()).to.be.instanceof( Promise );
+  });
+  it( 'will use a url parameter and a selector parameter', () => {
+    spy = sinon.spy( xrayWrapper, 'getValues' );
+    xrayWrapper.getValues( url, selectors );
+    expect( spy ).to.have.been.calledOnce;
+    expect( spy ).to.be.calledWithExactly( url, selectors );
   });
 });
